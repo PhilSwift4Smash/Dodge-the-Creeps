23a767e1c7248ae26edf4be09dd75b5129ce2809
Lehman_Zachary <lehmz719@hsd.k12.or.us>
2020-02-18 10:01:23 -0800

    stuff

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -1,5 +1,5 @@
 extends Node2D
-
+signal hit
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -7,12 +7,12 @@ extends Node2D
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	screen_size = get_viewport_rect().size
 	position = Vector2()
+	hide()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
@@ -45,3 +45,16 @@ func _process(delta):
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velcocity.y > 0
 	
+
+
+func _on_Player_hit():
+	pass # Replace with function body.
+	hide()  # Player disappears after being hit.
+func _on_Player_body_entered(body):
+	
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false
