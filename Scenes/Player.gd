d7637278e4d91de5527ca5156baaf3cf038b712c
Lehman_Zachary <lehmz719@hsd.k12.or.us>
2020-02-18 08:39:53 -0800

    bug fix

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -7,19 +7,18 @@ extends Node2D
 
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
-var velcocity = Vector2()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 	screen_size = get_viewport_rect().size
 	position = Vector2()
-	var velcocity = Vector2()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	var velcocity = Vector2()
 	
+	var velcocity = Vector2()
 	if Input.is_action_pressed("ui_up"):
 		velcocity.y += 1
 	if Input.is_action_pressed("ui_down"):
@@ -34,6 +33,6 @@ func _process(delta):
 	else:
 		$AnimatedSprite.stop()
 	
-	position += velocity * delta
+	position += velcocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
